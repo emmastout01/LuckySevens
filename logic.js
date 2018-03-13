d4dd12951e40dbce5b16bd71aaabaadf7c1f60c3
Emma Stout <emma.stout01@gmail.com>
2018-03-13 14:36:14 -0500

    return false in onsubmit function

--- logic.js
+++ logic.js
@@ -1,4 +1,8 @@
 function playGame() {
     var startingBet = document.forms['luckySevens']['startingBet'].value;
     console.log('starting bet', startingBet);
+    if (startingBet <= 0) {
+        alert('Error: Please enter a valid starting bet.')
+    }
+    return false;
 }
