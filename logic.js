2d5180db620aa8ef6e0636b476ce7824828a87a5
Emma Stout <emma.stout01@gmail.com>
2018-03-13 15:38:51 -0500

    added logic for highest amount and rolls at highest amount

--- logic.js
+++ logic.js
@@ -7,28 +7,39 @@ function playGame() {
     }
 
     var gameMoney = Number(startingBet);
+    var numberOfRolls = 0;
+    var highestAmountWon = 0;
+    var rollsAtHighest = 0;
     while (gameMoney > 0) {
+        numberOfRolls ++;
         var sumOfDice = rollDice();
         if (sumOfDice === 7) {
             gameMoney += 4;
+            if (gameMoney > highestAmountWon) {
+                highestAmountWon = gameMoney;
+                rollsAtHighest = numberOfRolls;
+            }
         }
         else {
             gameMoney -= 1;
         }
+       
         console.log('game money: ', gameMoney );
+        console.log('highest: ', highestAmountWon );
+        console.log('rolls at highest: ', rollsAtHighest);
+        
 
     }
     //Game over
-
+    return false;
+}
  
 
 
     function rollDice() {
         var die1 = Math.ceil(Math.random() * (1 + 6 - 1));
         var die2 = Math.ceil(Math.random() * (1 + 6 - 1));
-        console.log('sum of dice: ', die1 + die2 );
         return die1 + die2;
     }
 
-    return false;
-}
+  
